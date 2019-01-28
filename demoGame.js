28a2203e3b8cc674708bd6523d1aff92b7e2480e
Daniel Sabsay <dsabsay@calpoly.edu>
2019-01-27 19:26:00 -0800

    Debugging gh pages

--- demoGame.js
+++ demoGame.js
@@ -114,6 +114,7 @@ var background;
 
 /* Return the full path to a resource. */
 function getResourcePath(name) {
+    console.log(window.location.hostname);
     if (window.location.hostname.includes("dsabsay.github.io/ggj2019")) {
         return "https://raw.githubusercontent.com/dsabsay/ggj2019/master/" + name;
     }
